110b0c3c1178d645f2c0e6ff46c133eae07c9337
Ramareddy2002 <84448206+Ramareddy2002@users.noreply.github.com>
2021-05-20 12:00:17 +0530

    Add files via upload

---
+++ ppp2.c
@@ -0,0 +1,21 @@
+''' write a program that the user aks to read three values 
+    and total and average of the numbers
+
+
+a=int(input('enter value: '))
+b=int(input('enter value: '))
+c=int(input('enter value: '))
+total=a+b+c
+average=(sum)/3
+print('total = %d'%total)
+print('average = %.2f'%avgerage)
+
+
+'''
+expected output
+ enter value:2
+ enter value:4
+ enter value:6
+ total=12
+ average=4.00
+'''
